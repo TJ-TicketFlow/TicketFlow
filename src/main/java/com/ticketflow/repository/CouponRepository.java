43ad17c71dcaff786c0fd481b93e4018ae386f92
soohyun04 <yusuhyun04@gmail.com>
2026-06-18 17:14:34 +0900

    쿠폰명 기반 쿠폰 조회 메서드 추가 (멤버십 가입 쿠폰 발급용)

--- src/main/java/com/ticketflow/repository/CouponRepository.java
+++ src/main/java/com/ticketflow/repository/CouponRepository.java
@@ -3,5 +3,8 @@ package com.ticketflow.repository;
 import com.ticketflow.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    Optional<Coupon> findByCouponName(String couponName);
 }
