f75882bffdf8334ece9e6c6d47eef46deab39d3d
soohyun04 <yusuhyun04@gmail.com>
2026-06-18 17:23:05 +0900

    멤버십별 결제 내역 최신순 조회 메서드 추가

--- src/main/java/com/ticketflow/repository/MembershipPaymentRepository.java
+++ src/main/java/com/ticketflow/repository/MembershipPaymentRepository.java
@@ -1,7 +1,11 @@
 package com.ticketflow.repository;
 
+import com.ticketflow.entity.Membership;
 import com.ticketflow.entity.MembershipPayments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MembershipPaymentRepository extends JpaRepository<MembershipPayments, Long> {
+    List<MembershipPayments> findByMembershipOrderByMembershipHistoryDateDesc(Membership membership);
 }
