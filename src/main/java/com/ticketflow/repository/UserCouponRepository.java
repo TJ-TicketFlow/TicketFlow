1dda7575bbba22d6fc26380ea1a204f949e6df69
soohyun04 <yusuhyun04@gmail.com>
2026-06-18 17:35:17 +0900

    UserCouponRepository에 쿠폰 발급/만료/조회용 메서드 추가
    - existsByUserAndCoupon: 중복 발급 방지 체크용
    - findByUserCouponStatusAndUserCouponExpireAtBefore: 만료 쿠폰 배치 처리용
    - findByUserAndUserCouponStatus: 마이페이지 보유 쿠폰 조회용

--- src/main/java/com/ticketflow/repository/UserCouponRepository.java
+++ src/main/java/com/ticketflow/repository/UserCouponRepository.java
@@ -1,10 +1,16 @@
 package com.ticketflow.repository;
 
+import com.ticketflow.entity.Coupon;
+import com.ticketflow.entity.User;
 import com.ticketflow.entity.UserCoupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
     List<UserCoupon> findByUser_UserId(String userId);
+    boolean existsByUserAndCoupon(User user, Coupon coupon);
+    List<UserCoupon> findByUserCouponStatusAndUserCouponExpireAtBefore(Integer status, LocalDateTime dateTime);
+    List<UserCoupon> findByUserAndUserCouponStatus(User user, Integer userCouponStatus);
 }
