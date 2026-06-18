f3d0022e5f4952703f185f5c80a8722ea2a68996
soohyun04 <yusuhyun04@gmail.com>
2026-06-18 09:51:01 +0900

    MembershipRepository.java 이메일 기반의 멤버십 조회 메서드 추가

--- src/main/java/com/ticketflow/repository/MembershipRepository.java
+++ src/main/java/com/ticketflow/repository/MembershipRepository.java
@@ -1,10 +1,14 @@
 package com.ticketflow.repository;
 
 import com.ticketflow.entity.Membership;
+import com.ticketflow.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
     Optional<Membership> findTopByUser_UserIdOrderByMembershipCreatedAtDesc(String userId);
+    List<Membership> findByUser(User user);
+    Optional<Membership> findByUser_UserEmail(String userEmail);
 }
