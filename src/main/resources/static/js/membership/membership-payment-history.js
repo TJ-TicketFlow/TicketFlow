ceb3ef9b7789b4a9c52f784c4c263e92d18fff56
한승현 <hanseunghyun23@gmail.com>
2026-06-29 14:54:10 +0900

    06/29 좌석 선점후 취소했을 때 선점 상태 안풀리는 버그 수정

--- src/main/resources/static/js/membership/membership-payment-history.js
+++ src/main/resources/static/js/membership/membership-payment-history.js
@@ -1,1 +1,11 @@
-400: Invalid request
+function requestRefund() {
+    if (!confirm('환불을 신청하시겠습니까? 신청 시 멤버십이 즉시 해지됩니다.')) return;
+    fetch('/api/payment/refund', { method: 'POST' })
+        .then(res => res.json())
+        .then(data => {
+            alert(data.reason);
+            if (data.eligible) {
+                location.reload();
+            }
+        });
+}
